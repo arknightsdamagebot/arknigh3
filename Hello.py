6bb7b68e7259a3842e1d30e6b6e9e28f8f4de92e
arknightsdamagebot <167769981+arknightsdamagebot@users.noreply.github.com>
2024-07-21 13:50:34 +0000

    inu

--- Hello.py
+++ Hello.py
@@ -3,6 +3,7 @@ import numpy as np
 
 # マルコフモデルを使用して次の文字の確率を直前の文字に依存させる
 def generate_text_with_context(initial_sequence, transition_probs, num_chars=9):
+    generated_text = 'しかのこのこのここしたんたん'
     current_sequence = initial_sequence[-1]  # 最初の1文字を初期の直前の文字列とする
     
     for _ in range(num_chars - len(initial_sequence)):
